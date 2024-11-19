b1044f31bbc6d8d596a08f88d58720c24cfaacef
Dioges Cavalheiro <64334862+dicavalheiro@users.noreply.github.com>
2024-11-19 00:37:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -34,11 +34,41 @@ if vendedores != "Todos":
     df_filtered = df_filtered[df_filtered["Seller"] == vendedores]
 
 # Layout das colunas
+col01, col02, col03 = st.columns(3)
 col1, col2 = st.columns(2)
 col3, col4, col5 = st.columns(3)
-col6, = st.columns(1)
 
 # Gráficos
+col01.metric(
+    label="Total Faturamento",
+    value=f"{df_filtered['Total Sales'].sum():,.2f}".replace(",", "X").replace(".", ",").replace("X", "."),
+    delta=None,
+    delta_color='off',
+)
+
+col02.metric(
+    label="Total Unidades Vendidas",
+    value=f"{df_filtered['Units Sold'].sum():,.0f}".replace(",", "X").replace(".", ",").replace("X", "."),
+    delta=None,
+    delta_color="off",
+)
+
+# Garantir que o cálculo seja feito com valores agregados (sum)
+total_sales = df_filtered['Total Sales'].sum()
+operating_profit = df_filtered['Operating Profit'].sum()
+# Calcular a margem
+if total_sales != 0:  # Prevenir divisão por zero
+    margem = ((total_sales - operating_profit) / total_sales) * 100
+else:
+    margem = 0
+# Exibir a métrica
+col03.metric(
+    label="Margem (%)",
+    value=round(margem, 2),  # Arredondar para 2 casas decimais
+    delta=None,              # Você pode usar um delta se quiser comparar com outro valor
+    delta_color='normal'
+)
+
 fat_por_categoria = df_filtered.groupby('Product')['Total Sales'].sum().sort_values(ascending=False).reset_index()
 fig_cat = px.bar(fat_por_categoria, 
                  x='Product', 
@@ -47,25 +77,20 @@ fig_cat = px.bar(fat_por_categoria,
                  template='plotly_white')
 col1.plotly_chart(fig_cat, use_container_width=True)
 
-fat_por_regiao = df_filtered.groupby(['Region', 'Product'])['Total Sales'].sum().reset_index()
-fig_regiao = px.bar(fat_por_regiao, x="Region", y="Total Sales", color="Product", title="Faturamento por Região")
-col2.plotly_chart(fig_regiao, use_container_width=True)
+fat_por_catregiao = df_filtered.groupby(['Product', 'Region'])['Total Sales'].sum().sort_values(ascending=False).reset_index()
+fig_catregiao = px.bar(fat_por_catregiao, x="Product", y="Total Sales", color="Region", title="Faturamento por Categoria e Região")
+col2.plotly_chart(fig_catregiao, use_container_width=True)
 
-fat_vendedor = df_filtered.groupby("Seller")[["Total Sales"]].sum().reset_index()
-fig_vendedor = px.bar(fat_vendedor, x="Seller", y="Total Sales",
-                      title="Faturamento por Vendedor")
-col3.plotly_chart(fig_vendedor, use_container_width=True)
+fat_regiao = df_filtered.groupby("Region")[["Total Sales"]].sum().sort_values(by='Total Sales', ascending=False).reset_index()
+fig_regiao = px.bar(fat_regiao, x="Region", y="Total Sales",
+                      title="Faturamento por Região")
+col3.plotly_chart(fig_regiao, use_container_width=True)
 
 fig_kind = px.pie(df_filtered, values="Total Sales", names="Sales Method",
                   title="Faturamento por tipo de venda")
 col4.plotly_chart(fig_kind, use_container_width=True)
 
-mean_price = df_filtered.groupby('Seller')['Total Sales'].mean().sort_values(ascending=True).reset_index()
-fig_mean = px.bar(mean_price, x="Total Sales", y="Seller",
-                  title="Preço Médio por Vendedor")
+mean_price = df_filtered.groupby('Product')['Total Sales'].mean().sort_values(ascending=True).reset_index()
+fig_mean = px.bar(mean_price, x="Total Sales", y="Product",
+                  title="Preço Médio por Categoria")
 col5.plotly_chart(fig_mean, use_container_width=True)
-
-fat_mes = df_filtered.groupby(['Month', 'Seller'])['Total Sales'].sum().reset_index()
-fig_mes = px.line(fat_mes, x="Month", y="Total Sales", color="Seller", 
-                  title="Faturamento Mensal por Vendedor")
-col6.plotly_chart(fig_mes, use_container_width=True)
